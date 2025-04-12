c6ab2aab02f9d22624c30cdb22dcdf264166a223
Ravi Kumar Gupta <ravikumarg05082004@gmail.com>
2025-04-12 22:29:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors")
+
+const port = 5000;
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://book-store-frontend-three-tau.vercel.app'],
+  credentials: true
+}))
+
+const bookRoutes = require("./src/books/book.route")
+const orderRoutes = require("./src/orders/order.route")
+const userRoutes = require("./src/users/user.route")
+const adminRoutes = require("./src/stats/admin.stats")
+app.use("/api/books", bookRoutes)
+app.use("/api/orders", orderRoutes)
+app.use("/api/auth", userRoutes)
+app.use("/api/admin", adminRoutes)
+
+async function main() {
+  await mongoose.connect(process.env.DB_URL);
+  app.use("/", (req, res) => {
+    res.send("Book Store Server is Running");
+  });
+}
+
+main()
+  .then(()=>console.log("mongo db connected succesfully"))
+  .catch((err) => console.log(err));
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on ${port}`);
+});
