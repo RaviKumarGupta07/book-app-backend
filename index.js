fee09d26a34b459339e436f795154293915045e7
Ravi Kumar Gupta <ravikumarg05082004@gmail.com>
2025-04-12 22:59:18 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors")
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // middleware
